f3957030b6027d928a077b1f5a767ebeb32a4d75
RobertCL <RobertCL@iname.com>
2017-09-28 22:16:28 +0100

    Flesh out machineController.ino to use new libs and do the basic job of turning on/off a relay

--- machineController/machineController.ino
+++ machineController/machineController.ino
@@ -1,41 +1,82 @@
+/* Connections
+
+    CardReader522
+    - SDA => D4
+    - SCK => D5
+    - MOSI => D7
+    - MISO => D6
+    - IRQ => nc
+    - GND
+    - RST => D0
+    - 3.3V => 3.3V
+
+    Relay D3
+
+*/
+
+#include <ESP8266WiFi.h>
+
 #include <AccessSystem.h>
 #include <TokenCache.h>
+#include <CardReader522.h>
 
 #define THING_ID "1"
 
 AccessSystem accessSystem(THING_ID);
 TokenCache tokenCache(accessSystem);
+CardReader522 cardReader;
+
+TOKEN_CACHE_ITEM* item;
+
+unsigned long lastOn = 0;
 
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println("Machine controller v1");
+
+    cardReader.init();
     tokenCache.init();
 
-    TOKEN uid;  // Buffer to store the returned UID
-    uint8_t uidLength; // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    pinMode(D3, OUTPUT);
+    digitalWrite(D3, HIGH);
 
-    TOKEN_CACHE_ITEM* item;
-
-    item = tokenCache.fetch(&uid, uidLength, "TOKEN STRING");
-    
-    if (item != NULL) {
-        
-        if (item->flags && TOKEN_ACCESS) {
-            // permission given, so open/power the machine
-            item->count++;
-
-            Serial.print(F("Permission granted: "));
-            Serial.println(item->count);
-
-            // TODO: turn on power
-        }
+    Serial.print("Connecting wifi");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin("swindon-makerspace2", "makeallthethings");
+    while(WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
     }
+    Serial.println("Connected");
 }
-
 
 void loop()
 {
+    if (cardReader.check()) {
 
+        item = tokenCache.fetch(&cardReader.lastUID, cardReader.lastLen, cardReader.lastToken);
+        
+        if (item != NULL) {
+            if (item->flags && TOKEN_ACCESS) {
+                // permission given, so open/power the machine
+                item->count++;
+
+                Serial.print(F("Permission granted: "));
+                Serial.println(item->count);
+
+                Serial.println("turning relay on");
+                digitalWrite(D3, LOW);
+                lastOn = millis();
+            }
+        }
+        else
+        {
+            Serial.println("Token not found");
+        }
+    }
+
+    if (millis() - lastOn > 5000){
+        digitalWrite(D3, HIGH);
+    }
 }
-
- 
- 
